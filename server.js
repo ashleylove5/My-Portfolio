b4e30cbc8b349f14d5a19abdd3a727f32f7a4015
ashleylove5 <92226313+ashleylove5@users.noreply.github.com>
2025-03-04 11:13:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.post('/send', (req, res) => {
     service: 'gmail',
     auth: {
       user: 'ashleylove408@gmail.com', // replace with your email
-      pass: 'your-email-password' // replace with your email password
+      pass: 'AshleyMaluleke@2001' // replace with your email password
     }
   });
 
